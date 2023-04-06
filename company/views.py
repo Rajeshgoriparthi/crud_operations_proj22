2a068bde1e21dbb0cc2335ebd11e61a83646a01b
Rajeshgoriparthi <goriparthiraj77@gmail.com>
2023-04-06 18:55:25 +0530

    first commit

--- company/views.py
+++ company/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from company.models import *
+from django.db.models.functions import Length
 # Create your views here.
 
 def back_to_frnt(request):
@@ -11,5 +12,18 @@ def back_to_frnt(request):
 
 def emp_table(request):
     Loe=employe.objects.all()
+    Loe=employe.objects.filter(ename='Rajesh')
+    Loe=employe.objects.order_by('ename')
+    Loe=employe.objects.order_by('-ename')
+    Loe=employe.objects.exclude(ename='Rajesh')
+    Loe=employe.objects.all()
+    Loe=employe.objects.filter(age__gt='22')
+    Loe=employe.objects.filter(emp_id__gt='1001')
+    Loe=employe.objects.filter(emp_id__gt='1002')
+    Loe=employe.objects.filter(age__gte='24')
+    Loe=employe.objects.filter(age__lt='23')
+    Loe=employe.objects.filter(age__lte='23')
+    Loe=employe.objects.order_by(Length('ename'))
+    Loe=employe.objects.order_by(Length('ename').desc())
     d1={'edata':Loe}
     return render(request,'emp_table.html',d1)
